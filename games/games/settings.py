40b4f7d6bd33a46eece2ce5f519b3837c6973fae
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-12 17:20:53 -0500

    adding setting up the URL django files for the project and the app

--- games/games/settings.py
+++ games/games/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'matcha_matches',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ games/matcha_matches/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("tic-tac-toe", views.ticTacToe, names="tic-tac-toe"),
+    path("tic-tac-tea", views.ticTacoTea, name="tic-tac-tea"),
+    path("checkers", views.checkers, name="checkers"),
+    path("checkers-chai", views.chai, name="checkers-chai"),
+    path("4-in-a-row", views.connect, name="connect"),
+    path("coco-connect", views.cocoConnect, name="coco-connect"),
+]
