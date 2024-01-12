ff1f8f06156273f296c84fcaf4a1edf41bbf181a
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-12 18:02:42 -0500

    Added some comments to views.py for clarity

--- games/matcha_matches/views.py
+++ games/matcha_matches/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Handles the home page for matcha matches
 def index(request):
     return render(request, "matcha_matches/index.html")
 
+
+# Redirects the user to the normal tic-tac-toe application
 def ticTacToe(request):
     return
 
+
+# Redirects the user to the "twisted" tic-tac-toe application, AKA tic-tac-tea
 def ticTacTea(request):
     return
 
+
+# Redirects the user to the normal checkers application
 def checkers(request):
     return
     
     
+# Redirects the user to the "twisted checkers" application, AKA Checkers chai
 def chai(requset):
     return
 
 
+# Redirects user to the 4-in-a-row application
 def connect(request):
     return
 
+
+# Redirects user to the Coco Connect application
 def cocoConnect(request):
     return
